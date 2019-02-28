fa4b1787e44677f1fe72ae8871c50d96461f058e
Joseph S <38223151+Jodastt@users.noreply.github.com>
2019-02-28 09:42:08 -0800

    Create cubiczeros.rkt
    computes zeros of cubic function

---
+++ algorithms/cubiczeros.rkt
@@ -0,0 +1,44 @@
+#lang racket
+(define tolerance 0.00001)
+
+(define (average a b)
+  (/ (+ a b) 2))
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2))
+       tolerance))
+  (define (try guess)
+    (display guess) (newline)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+
+(define dx 0.00001)
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x)
+            ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g)
+               guess))
+
+(define (cubic a b c)
+  (lambda (x)
+    (+ (* x x x) ( * a x x) (* b x) c)))
+
+(define a 1)
+(define b 1)
+(define c 1)
+
+(display (newtons-method (cubic a b c) 1))
