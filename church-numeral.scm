2f44caf61ec239d04a5db44c58959e333f834fee
Joseph S <38223151+Jodastt@users.noreply.github.com>
2019-03-05 14:47:27 -0800

    Update church-numeral.scm
    Step complexity is O(1)
    Time complexity is O(n)

--- church-numeral.scm
+++ church-numeral.scm
@@ -1,6 +1,16 @@
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+(define one (lambda (f) (f (f x))))
+
+(define (add m n)
+  (lambda (f) (lambda (x) ((m f) ((n f) x)))))
+
 (define (church-numeral x)
   (define (iter result x count)
     (if (< count x) 
-    (iter (add-1 result x (+ 1 count)))
-    count))
+    (iter (add-1 result) x (+ 1 count)))
+    result)
   (iter zero x 0))
