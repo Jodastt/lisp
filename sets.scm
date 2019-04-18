be102e86804e46e0a9de4cd956b4da43a7b6622e
Joseph S <38223151+Jodastt@users.noreply.github.com>
2019-04-18 10:11:15 -0700

    Update sets.scm

--- sets.scm
+++ sets.scm
@@ -1,6 +1,6 @@
 (define (element-of-set? x set)
   (cond ((null? set) #f)
-        ((equal? x (car set)) true)
+        ((equal? x (car set)) #t)
         (else (element-of-set? x (cdr set)))))
 (define (adjoin-set x set)
   (if (element-of-set? x set)
